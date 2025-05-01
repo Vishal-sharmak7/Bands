bf451caa49a1bd08b179d4570c5d40e5bfa751ec
Vishal-sharmak7 <vishalsharmak7@gmail.com>
2025-05-01 21:10:24 +0530

    vercel

--- backend/controllers/concert.user.js
+++ backend/controllers/concert.user.js
@@ -1,6 +1,7 @@
 import {concert} from "../models/user.model.js";
 const concertInfo = async (req, res) => {
 
+  console.log("HELLO")
   const data = await concert.find(req.query);
   res.status(200).json(data);
   
